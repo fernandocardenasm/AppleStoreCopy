a9d968e724d010db31cee1bdded47d0fedb14cda
fernandocardenasm <fernando.cardenasm@gmail.com>
2017-01-18 22:35:45 +0100

    Add elements for each app cell

--- AppleStore/CategoryCell.swift
+++ AppleStore/CategoryCell.swift
@@ -26,23 +26,43 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         layout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Best New Apps"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.4, alpha: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     func setupViews() {
         backgroundColor = .clear
         
         addSubview(appsCollectionView)
-        
-        appsCollectionView.backgroundColor = .red
+        addSubview(dividerLineView)
+        addSubview(nameLabel)
+
+        appsCollectionView.backgroundColor = .clear
         
         appsCollectionView.delegate = self
         appsCollectionView.dataSource = self
         appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: cellId)
         
+        
         addConstrainstWithFormat("H:|[v0]|", views: appsCollectionView)
-        addConstrainstWithFormat("V:|[v0]|", views: appsCollectionView)
-
+        addConstrainstWithFormat("H:|[v0]|", views: nameLabel)
+        addConstrainstWithFormat("H:|[v0]|", views: dividerLineView)
+        addConstrainstWithFormat("V:|[v0(30)]-2-[v1][v2(0.5)]|", views: nameLabel, appsCollectionView, dividerLineView)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -55,7 +75,7 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(100, frame.height)
+        return CGSize(100, frame.height - 40)
     }
 }
 
@@ -91,6 +111,17 @@ class AppCell: UICollectionViewCell {
     let categoryLabel: UILabel = {
         let label = UILabel()
         label.text = "Entertainment"
+        label.textColor = .gray
+        label.backgroundColor = .yellow
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "3.99$"
+        label.textColor = .gray
         label.backgroundColor = .yellow
         label.font = UIFont.systemFont(ofSize: 13)
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -101,11 +132,14 @@ class AppCell: UICollectionViewCell {
         addSubview(imageView)
         addSubview(nameLabel)
         addSubview(categoryLabel)
+        addSubview(priceLabel)
         
         addConstrainstWithFormat("H:|[v0]|", views: imageView)
-        addConstrainstWithFormat("V:[v0(100)]-2-[v1(40)]-2-[v2(20)]", views: imageView, nameLabel, categoryLabel)
+        addConstrainstWithFormat("V:[v0(100)]-2-[v1(40)]-2-[v2(20)]-2-[v3(20)]", views: imageView, nameLabel, categoryLabel, priceLabel)
         addConstrainstWithFormat("H:|[v0]|", views: nameLabel)
         addConstrainstWithFormat("H:|[v0]|", views: categoryLabel)
+        addConstrainstWithFormat("H:|[v0]|", views: priceLabel)
+
         
         //addConstrainstWithFormat("V:|[v0(80)]-2-[v1]-2-|", views: imageView, nameLabel)
         //addConstrainstWithFormat("H:|[v0]|", views: nameLabel)
