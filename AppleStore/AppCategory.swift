614f5504148dd7bf4a230c20746049a87799bb7f
fernandocardenasm <fernando.cardenasm@gmail.com>
2017-02-10 16:14:47 +0100

    New structure for the folders and files

--- AppleStore/AppCategory.swift
+++ AppleStore/AppCategory.swift
@@ -16,51 +16,6 @@ struct AppCategory{
     
     var apps: [App]?
     
-    
-//    static func sampleAppCategories() -> [AppCategory] {
-//    
-//
-//        var bestNewAppCategory = AppCategory()
-//        bestNewAppCategory.name = "Best new Apps"
-//        
-//        
-//        //logic
-//        var frozenApp = App()
-//        frozenApp.name = "Disney Build It: Frozen"
-//        frozenApp.imageName = "frozen"
-//        frozenApp.category = "Entertainment"
-//        frozenApp.price = NSNumber(value: 3.99)
-//        
-//        bestNewAppCategory.apps = [App]()
-//        bestNewAppCategory.apps?.append(frozenApp)
-//
-//        
-//        var bestNewGamesCategory = AppCategory()
-//        bestNewGamesCategory.name = "Best new Games"
-//        
-//        bestNewGamesCategory.apps = [App]()
-//
-//        //logic
-//        var telepaintApp = App()
-//        telepaintApp.name = "Telepaint"
-//        telepaintApp.imageName = "telepaint"
-//        telepaintApp.category = "Games"
-//        telepaintApp.price = NSNumber(value: 2.99)
-//        
-//        var starwarsApp = App()
-//        starwarsApp.name = "Star Wars"
-//        starwarsApp.imageName = "starwars"
-//        starwarsApp.category = "Games"
-//        starwarsApp.price = NSNumber(value: 5.99)
-//        
-//        bestNewGamesCategory.apps?.append(telepaintApp)
-//        bestNewGamesCategory.apps?.append(starwarsApp)
-//        
-//        
-//        return [bestNewAppCategory, bestNewGamesCategory]
-//    
-//    }
-    
 }
 
 struct App {
